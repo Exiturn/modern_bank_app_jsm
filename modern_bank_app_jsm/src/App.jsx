612451945b8aee5471aa518ae5d66797f6f88835
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-01-19 23:27:06 +0000

    BM - Progress on Navbar Component
    -Created the file structure for the project
    -Started on the navbar component

--- modern_bank_app_jsm/src/App.jsx
+++ modern_bank_app_jsm/src/App.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import style from './style.js'
+
+import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials,
+Clients, CTA, Footer } from './components'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='bg-primary w-full overflow-hidden'>
+      {/* Navbar */}
+      <div className={`${style.paddingX} ${style.flexCenter}`}>
+        <div className={`${style.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      {/* Hero */}
+      <div className={`bg-primary ${style.flexStart}`}>
+        <div className={`${style.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      {/* Content */}
+      <div className={`bg-primary ${style.paddingX} ${style.flexStart}`}>
+        <div className={`${style.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer /> 
+        </div>
+      </div>
+    </div>
   )
 }
 
---
+++ modern_bank_app_jsm/src/components/index.js
@@ -0,0 +1,28 @@
+import Navbar from "./Navbar"
+import Billing from "./Billing"
+import Business from "./Business"
+import Button from "./Button"
+import CardDeal from "./CardDeal"
+import Clients from "./Clients"
+import CTA from "./CTA"
+import Feedback from "./Feedback"
+import Stats from "./Stats"
+import Testimonials from "./Testimonials"
+import Hero from "./Hero"
+import Footer from "./Footer"
+
+
+export {
+    Navbar,
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    Feedback,
+    Stats,
+    Testimonials,
+    Hero,
+    Footer
+}
